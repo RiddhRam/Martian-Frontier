a0660fb78b896dcdd6a4d1d09f5baaee1f17963d
RiddhRam <ridderp05@gmail.com>
2024-10-23 12:25:42 -0400

    Done setting up tilemap for Level 1 rock

---
+++ Assets/Scripts/MineRenderer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MineRenderer : MonoBehaviour
+{
+    public Tilemap mineTilemap;
+    public RuleTile level1Rock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
