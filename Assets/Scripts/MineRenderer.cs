458e3e2b27008f56d0c68b49412f6631616e05f6
RiddhRam <ridderp05@gmail.com>
2024-11-07 17:18:58 -0500

    Vision algorithm works properly

--- Assets/Scripts/MineRenderer.cs
+++ Assets/Scripts/MineRenderer.cs
@@ -4,6 +4,8 @@ using UnityEngine.Tilemaps;
 
 public class MineRenderer : MonoBehaviour
 {
+    // Have to change through hierarchy not through here
+    public int visionRadius = 20;
     public GameObject mineTilemapPrefab;  // Reference to the Tilemap component
     public TileBase unknownTile;
     public RuleTile level1Rock;  // Tile to place on the grid
@@ -36,13 +38,13 @@ public class MineRenderer : MonoBehaviour
             CreateTiles(i);
         }
 
+        // Reveal the entry blocks
         RevealTile(new(-3, -5));
         RevealTile(new(-2, -5));
         RevealTile(new(-1, -5));
         RevealTile(new(0, -5));
         RevealTile(new(1, -5));
         RevealTile(new(2, -5));
-
     }
 
     // Places tiles in a 25x12 rectangle, starting from (-50, -5) and going to the right and downward
@@ -88,7 +90,7 @@ public class MineRenderer : MonoBehaviour
             }
 
             // Now place ore veins throughout the chunk
-            int oreVeinCount = Random.Range(1, 3);
+            int oreVeinCount = Random.Range(3, 5);
             GenerateOreVeins(chunkTileCoordinates, oreVeinCount, i, chunkRow);
         }
 
@@ -103,10 +105,10 @@ public class MineRenderer : MonoBehaviour
             // Randomly choose the center position for each vein within the chunk
             int centerX = Random.Range(0, gridSize.x);
             int centerY = Random.Range(0, gridSize.y);
-            int radius = Random.Range(2, 4); // Radius of 3-5 tiles for variation
+            int radius = Random.Range(2, 5); // Radius of 2-6 tiles for variation
 
             // Select an ore based on the depth (chunkRow) to increase the chances of higher-value ores
-            int oreToPlace = SelectOreBasedOnDepth(chunkRow);
+            int oreToPlace = SelectOreBasedOnDepth(chunkRow, 1);
 
             for (int x = -radius; x <= radius; x++)
             {
@@ -134,16 +136,18 @@ public class MineRenderer : MonoBehaviour
     }
 
     // Method to select an ore based on depth
-    private int SelectOreBasedOnDepth(int chunkRow)
+    private int SelectOreBasedOnDepth(int chunkRow, int level)
     {
         // Calculate the probability weights based on depth
-        float depthFactor = Mathf.Clamp01(chunkRow / 20f);  // Lower 10f to make the rarity change faster, increase to change it slower
+        float depthFactor = Mathf.Clamp01(chunkRow / 10f);  // Lower 20f to make the rarity change faster, increase to change it slower
 
-        // Select an index between 0 and the highest array index. Random.Value can add 1 to the index, or it adds 0, no in between since the index will be an integer
-        int selectedOreIndex = (int) Mathf.Clamp(depthFactor * level1Ores.Length + Random.value, 0, level1Ores.Length - 1);
+        // Select an index 
+        // Random.Value can add 1 to the index, or it adds 0, no in between since the index will be an integer
+        // Must be between 1-3 (level 1), 5-7 (level 2), 9-11 (level 3)
+
+        int selectedOreIndex = (int) Mathf.Clamp(depthFactor * 3 + Random.value, 1, 3);
         
         // Higher depths increase chances for rarer ores at the end of the array
-        //return level1Ores[selectedOreIndex];
         return selectedOreIndex;
     }
 
@@ -151,6 +155,35 @@ public class MineRenderer : MonoBehaviour
         int tilemapIndex = Mathf.FloorToInt((tilePos.y + 5) / -12f);
 
         tilemaps[tilemapIndex].SetTile(new(tilePos.x, tilePos.y), tileValues[tilemapsTileValues[tilemapIndex][new(tilePos.x, tilePos.y)]]);
+    }
+
+    public void DestroyTile(Vector3Int tileToDestroy) {
+        int tilemapIndex = Mathf.FloorToInt((tileToDestroy.y + 5) / -12f);
+        tilemapIndex = Mathf.Clamp(tilemapIndex, 0, 35);
+
+        Tilemap tilemap = tilemaps[tilemapIndex];
+
+        tilemap.SetTile(tileToDestroy, null);
+        tilemapsTileValues[tilemapIndex].Remove(new(tileToDestroy.x, tileToDestroy.y));
+
+        // Search in a radius around tileToDestroy
+        for (int x = -visionRadius; x <= visionRadius; x++) {
+            for (int y = -visionRadius; y <= visionRadius; y++) {
+                Vector2Int checkPos = new(tileToDestroy.x + x, tileToDestroy.y + y);
+
+                tilemapIndex = Mathf.FloorToInt((tileToDestroy.y + y + 5) / -12f);
+                tilemapIndex = Mathf.Clamp(tilemapIndex, 0, 35);
+                
+                // Check if the tile exists in tilemapsTileValues
+                if (tilemapsTileValues[tilemapIndex].ContainsKey(checkPos)) {
+                    RevealTile(checkPos);
+                }
+            }
+        }
+    }
+
+    public TileBase[] GetOres() {
+        return new TileBase[] { IronOre, SulfurOre, LimestoneRock };
     }
 }
 
