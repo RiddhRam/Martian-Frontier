1e7dbb6d6e9c450221d1ba252ea780949d785e80
RiddhRam <ridderp05@gmail.com>
2024-12-09 22:58:33 -0500

    Fixed problem with wheels not turning

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -108,7 +108,7 @@ public class PlayerMovement : MonoBehaviour
     public void SetSpeed(float newSpeed) {
         playerSpeed = newSpeed;
 
-        Transform vehicle = transform.GetChild(1);
+        Transform vehicle = transform.GetChild(0);
         // SetSpeed is called when a new vehicle is placed
         // When a new vehicle is placed we should also check if it needs animated wheels or not
         for (int i = 0; i != vehicle.childCount; i++) {
@@ -117,7 +117,6 @@ public class PlayerMovement : MonoBehaviour
                 return;
             }
         }
-
         frontWheels = null;
     }
 }
