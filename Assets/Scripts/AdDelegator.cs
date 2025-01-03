56dd1cf6faf1cc93e27f973f1b3df5ce8f2cfbea
RiddhRam <ridderp05@gmail.com>
2025-01-02 22:07:32 -0500

    App is ready for iOS production

--- Assets/Scripts/AdDelegator.cs
+++ Assets/Scripts/AdDelegator.cs
@@ -106,7 +106,7 @@ public class AdDelegator : MonoBehaviour, IDataPersistence
             }
             else
             {
-                _adUnitId = "ca-app-pub-3940256099942544/1712485313"; // iOS Real Ad Unit
+                _adUnitId = "ca-app-pub-5607588731152504/4737462608"; // iOS Real Ad Unit
             }
         #else
             _adUnitId = "unused"; // Default for other platforms
