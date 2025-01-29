0af23f477a4bcdda4ebe8ef4cd2e2e641eee3557
RiddhRam <ridderp05@gmail.com>
2025-01-29 15:06:28 -0500

    Setup the base design of daily challenges panel, didn't write any scripts

--- Assets/Scripts/AskForReview.cs
+++ Assets/Scripts/AskForReview.cs
@@ -37,8 +37,6 @@ public class AskForReview : MonoBehaviour
             } else if (screenType == "Public Review") {
                 StartCoroutine(RequestForReviews());
                 GameObject.Find("Analytics Delegator").GetComponent<AnalyticsDelegator>().EnjoyingGame();
-                
-                Destroy(gameObject);
             }
             
         }
@@ -57,16 +55,11 @@ public class AskForReview : MonoBehaviour
         }
         else if (responseTracker == 1) {
             Destroy(gameObject);
-
         }
     }
 
-    public void Test() {
-        StartCoroutine(RequestForReviews());
-    }
-
     private IEnumerator RequestForReviews() {
-        Debug.Log("REQUESTING REVIEW");
+
         #if UNITY_ANDROID
             ReviewManager _reviewManager = new();
             PlayReviewInfo _playReviewInfo;
@@ -91,6 +84,9 @@ public class AskForReview : MonoBehaviour
         #elif UNITY_IPHONE
             UnityEngine.iOS.Device.RequestStoreReview();
         #endif
+
+        yield return new WaitForSeconds(1f);
+        gameObject.SetActive(false);
 
         yield break;
     }
