ed7bbaa0f81d3de4fe3a293ff7b244f8c16ade58
RiddhRam <ridderp05@gmail.com>
2025-03-15 17:56:12 -0400

    Couldnt optimize navmesh updates, so just reduced the frequency for now.

--- Assets/Scripts/BuildNavMeshSurface.cs
+++ Assets/Scripts/BuildNavMeshSurface.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(NavMeshSurface))]
 public class BuildNavMeshSurface : MonoBehaviour
 {
-    public NavMeshSurface[] navMeshSurfaces;
+
+    [SerializeField]
+    private MineRenderer mineRenderer;
+    [SerializeField]
+    private LayerMask includedLayers;
+    [SerializeField]
+    private NavMeshSurface[] navMeshSurfaces;
+    
     public bool UpdateMeshes;
+    private readonly Bounds bounds = new(new(-80, 0, 0), new(80, 550, 100));
     
     // Start is called before the first frame update
     public void InitializeMesh()
@@ -18,7 +26,7 @@ public class BuildNavMeshSurface : MonoBehaviour
         foreach (var surface in navMeshSurfaces)
         {
             surface.hideEditorLogs = true;
-            surface.BuildNavMesh();
+            surface.BuildNavMesh();           
         }
         
         StartCoroutine(UpdateNavMeshCoroutine());
@@ -28,12 +36,33 @@ public class BuildNavMeshSurface : MonoBehaviour
         while (true) {
             yield return new WaitForSeconds(3);
 
+            /*
+            int index = 1;
             foreach (var surface in navMeshSurfaces)
-            {
+            {   
+                Debug.Log("Updating: " + surface.name);
+
+                List<NavMeshBuildSource> sources = new();
+                
+                NavMeshBuilder.CollectSources(surface.navMeshData.sourceBounds, includedLayers, NavMeshCollectGeometry.PhysicsColliders, 0, new List<NavMeshBuildMarkup>(), sources);
+                //surface.UpdateNavMesh(surface.navMeshData);
+                var async = NavMeshBuilder.UpdateNavMeshDataAsync(surface.navMeshData, NavMesh.GetSettingsByIndex(index), sources, surface.navMeshData.sourceBounds);
+
+                yield return async;
+
+                index++;
+
+                yield return new WaitForSeconds(10);
+            }*/
+
+            foreach (var surface in navMeshSurfaces)
+            {   
                 surface.UpdateNavMesh(surface.navMeshData);
 
-                yield return new WaitForSeconds(30);
+                yield return new WaitForSeconds(60);
             }
+
+
         }
 
     }
