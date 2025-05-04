91661eb65982f9b0bff9c4b63e6efd1f86f2d340
RiddhRam <ridderp05@gmail.com>
2025-05-04 11:34:34 -0400

    Branch comment: [Revamping game progression from tier based to upgrade based] Refactored garage

--- Assets/Scripts/GarageDelegator.cs
+++ Assets/Scripts/GarageDelegator.cs
@@ -32,7 +32,7 @@ public class GarageDelegator : MonoBehaviour, IDataPersistence
 
     public SerializableDictionary<string, int> vehicleUpgradeLevels;
     private readonly int[] upgradeGemPrices = new int[] {20, 40, 80, 150, 240, 360, 490, 660, 840, 1000, 1300, 1500, 1800, 2100, 2400, 2800, 3200, 3600, 4000, 4400, 4900, 5400, 5900, 6400, 7000, 7600, 8200, 8800, 9500, 10200, 10900, 11600, 12400, 13100, 13900, 14700, 15600, 16500, 17400, 18300, 19200, 20200, 21200, 22200, 23200, 24300, 25300, 26400, 27600, 28700, 29900, 31100, 32300, 33600, 34800, 36100, 37400, 38800, 40100, 41500, 42900, 44400, 45800, 47300, 48800, 50300, 51900, 53500, 55100, 56700, 58300, 60000, 61700, 63400, 65200, 66900, 68700, 70500, 72400, 74200, 76100, 78000, 79900, 81900, 83900, 85900, 87900, 89900, 92000, 94100, 96200, 98400, 101000, 103000, 105000, 107000, 109000, 112000, 114000, 116000, 119000, 121000, 123000, 126000, 128000, 131000, 133000, 136000, 138000, 141000, 144000, 146000, 149000, 151000, 154000, 157000, 160000, 162000, 165000, 168000, 171000, 174000, 176000, 179000, 182000, 185000, 188000, 191000, 194000, 197000, 200000, 203000, 206000, 210000, 213000, 216000, 219000, 222000, 226000, 229000, 232000, 235000, 239000, 242000, 246000, 249000, 252000, 256000, 259000, 263000, 266000, 270000, 274000, 277000, 281000, 284000, 288000, 292000, 296000, 299000, 303000, 307000, 311000, 314000, 318000, 322000, 326000, 330000, 334000, 338000, 342000, 346000, 350000, 354000, 358000, 362000, 367000, 371000, 375000, 379000, 383000, 388000, 392000, 396000, 401000, 405000, 409000, 414000, 418000, 423000, 427000, 432000, 436000, 441000, 445000, 450000, 454000, 459000, 464000, 468000};
-    public Image stubbyImage;
+    private Image stubbyImage;
     public bool blockPanelSwitching;
 
     public void DeactivatePanel() {
@@ -72,24 +72,6 @@ public class GarageDelegator : MonoBehaviour, IDataPersistence
         // If drillers
         if (panelToActivate == "Drillers") {
 
-            GameObject[] tierPanels = new GameObject[tierColors.Length];
-            // Create a tier panel for each tier
-            for (int i = 0; i != tierPanels.Length; i++) {
-                GameObject newTierPanel = Instantiate(drillerTierPanel);
-                tierPanels[i] = newTierPanel;
-                Transform panelTransform = tierPanels[i].transform;
-                panelTransform.SetParent(drillersContent.transform);
-                // Have to make sure scale is right
-                panelTransform.localScale = new(1, 1, 1);
-                // Get the right translation
-                string tierString = GetLocalizedValue("TIER {0}", i+1);
-                // Set the name
-                panelTransform.GetChild(0).GetComponent<TextMeshProUGUI>().text = tierString;
-            }
-
-            // Track number of items in each tier, to dynamically resize content height based on rows
-            int[] tierItems = new int[tierPanels.Length];
-
             for (int i = 0; i != drillers.Length; i++) {
 
                 // Get the prefab and make its panel
@@ -103,7 +85,7 @@ public class GarageDelegator : MonoBehaviour, IDataPersistence
                 Transform panelTransform = newVehiclePanel.transform;
                 // Add panel to the content scroll view of the right tier panel
                 // This should just be a regular panel with a photo
-                panelTransform.SetParent(tierPanels[tier - 1].transform.GetChild(1));
+                panelTransform.SetParent(drillersContent.transform);
 
                 panelTransform.localScale = new(1, 1, 1);
 
@@ -116,22 +98,10 @@ public class GarageDelegator : MonoBehaviour, IDataPersistence
                 panelTransform.GetChild(1).GetComponent<Image>().sprite = drillersImages[i];
                 panelTransform.GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>().text = "$" + FormatPrice(price);
 
-                if (level == upgradeGemPrices.Length) {
-                    panelTransform.GetChild(4).GetChild(0).gameObject.SetActive(false);
-                    panelTransform.GetChild(4).GetChild(1).gameObject.SetActive(true);
-                    panelTransform.GetChild(4).GetComponent<Button>().interactable = false;;
-                } else {
-                    panelTransform.GetChild(4).GetChild(0).GetChild(1).GetComponent<TextMeshProUGUI>().text = FormatPrice(upgradeGemPrices[level]);
-                }
-
-                panelTransform.GetChild(5).GetComponent<TextMeshProUGUI>().text = drillers[i].name;
-                TextMeshProUGUI levelText = panelTransform.GetChild(6).GetComponent<TextMeshProUGUI>();
-                TextMeshProUGUI profitText = panelTransform.GetChild(7).GetComponent<TextMeshProUGUI>();
-                levelText.text = GetLocalizedValue("LEVEL {0}", level);
-                profitText.text = GetLocalizedValue("PROFIT: +{0}%", GetVehicleProfitMultiplier(drillers[i].name) * 100);
-                panelTransform.GetChild(8).GetChild(1).GetComponent<TextMeshProUGUI>().text = drillerController.endurance.ToString();
-                panelTransform.GetChild(9).GetChild(1).GetComponent<TextMeshProUGUI>().text = drillerController.width.ToString();
-                panelTransform.GetChild(10).GetChild(1).GetComponent<Slider>().value = drillSpeed;
+                panelTransform.GetChild(4).GetComponent<TextMeshProUGUI>().text = drillers[i].name;
+                panelTransform.GetChild(5).GetChild(1).GetComponent<TextMeshProUGUI>().text = drillerController.endurance.ToString();
+                panelTransform.GetChild(6).GetChild(1).GetComponent<TextMeshProUGUI>().text = drillerController.width.ToString();
+                panelTransform.GetChild(7).GetChild(1).GetComponent<Slider>().value = drillSpeed;
 
                 // I made some changes, these comments might be wrong
                 // Multiply the width and height of the panel image relative to the proportion of 
@@ -144,9 +114,7 @@ public class GarageDelegator : MonoBehaviour, IDataPersistence
                 float scaleFactor = drillersImages[i].bounds.size.x / 2.89f * drillers[i].transform.localScale.x;
 
                 panelTransform.GetChild(1).transform.localScale = new(scaleFactor, 1.16f * scaleFactor, 1);
-                
-                tierItems[tier-1]++;
-
+            
                 // Get the Buy Button component
                 Button buyButton = panelTransform.GetChild(2).GetComponent<Button>();
                 // Have to save it as a variable with a local scope, or else it keeps going up and out of bounds
@@ -160,32 +128,19 @@ public class GarageDelegator : MonoBehaviour, IDataPersistence
                     // Add an OnClick listener to the button and pass in the prefab of the vehicle
                     buyButton.onClick.AddListener(() => OnBuyButtonClick(newVehiclePanel, drillers[index]));
                 }
-
-                // Get the Buy Button component
-                Button upgradeButton = panelTransform.GetChild(4).GetComponent<Button>();
-                // Add an OnClick listener to the button and pass in the prefab of the vehicle
-                upgradeButton.onClick.AddListener(() => OnUpgradeButtonClick(drillers[index].name, panelTransform.GetChild(4), levelText, profitText));
             }
 
-            float bigContentHeight = 0;
-            // Resize each tier panel
-            for (int i = 0; i != tierPanels.Length; i++) {
-                Transform scrollViewContent = tierPanels[i].transform.GetChild(1);
-                // Calculate the number of rows
-                GridLayoutGroup gridLayoutGroup = scrollViewContent.GetComponent<GridLayoutGroup>();
-                int columns = Mathf.Max(1, Mathf.FloorToInt(scrollViewContent.GetComponent<RectTransform>().rect.width / gridLayoutGroup.cellSize.x));
-                int rows = Mathf.CeilToInt((float) tierItems[i] / columns);
+            Canvas.ForceUpdateCanvases();
 
-                // Resize the scroll view content height to fit the rows (top padding of tier panels + cell height * rows + vertical spacing between cell rows * (rows - 1))
-                RectTransform contentRect = scrollViewContent.GetComponent<RectTransform>();
-                contentRect.sizeDelta = new Vector2(contentRect.sizeDelta.x, 50 + 2550 * rows + 40 * (rows - 1));
-                tierPanels[i].GetComponent<RectTransform>().sizeDelta = new (0, contentRect.sizeDelta.y);
-                bigContentHeight += contentRect.sizeDelta.y;
-            }
+            GridLayoutGroup drillerGridLayoutGroup = drillersContent.GetComponent<GridLayoutGroup>();
+            int drillerColumns = Mathf.Max(1, Mathf.FloorToInt(drillersContent.GetComponent<RectTransform>().rect.width / drillerGridLayoutGroup.cellSize.x));
+            int drillerRows = Mathf.CeilToInt((float) drillers.Length / drillerColumns);
 
-            RectTransform bigContentRect = drillersContent.GetComponent<RectTransform>();
-            // Resize the scroll view content height to fit the rows using the height of all panels and then factor in the spacing * (tiers - 1) which is (150 * 2) currently
-            bigContentRect.sizeDelta = new Vector2(bigContentRect.sizeDelta.x, 100 + bigContentHeight + 150 * (tierPanels.Length - 1));
+            RectTransform drillersContentRect = drillersContent.GetComponent<RectTransform>();
+            // Resize the scroll view content height to fit the rows (top padding + cell height * rows + vertical spacing between cell rows * (rows - 1))
+            drillersContentRect.sizeDelta = new Vector2(drillersContentRect.sizeDelta.x, 50 + 2000 * drillerRows + 40 * (drillerRows - 1));
+            drillersContent.GetComponent<RectTransform>().sizeDelta = new (0, drillersContentRect.sizeDelta.y);
+
             return;
         }
 
@@ -211,22 +166,10 @@ public class GarageDelegator : MonoBehaviour, IDataPersistence
             panelTransform.GetChild(1).GetComponent<Image>().sprite = haulersImages[i];
             panelTransform.GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>().text = "$" + FormatPrice(price);
 
-            if (level == upgradeGemPrices.Length) {
-                panelTransform.GetChild(4).GetChild(0).gameObject.SetActive(false);
-                panelTransform.GetChild(4).GetChild(1).gameObject.SetActive(true);
-                panelTransform.GetChild(4).GetComponent<Button>().interactable = false;;
-            } else {
-                panelTransform.GetChild(4).GetChild(0).GetChild(1).GetComponent<TextMeshProUGUI>().text = FormatPrice(upgradeGemPrices[level]);
-            }
-
-            panelTransform.GetChild(5).GetComponent<TextMeshProUGUI>().text = haulers[i].name;
-            TextMeshProUGUI levelText = panelTransform.GetChild(6).GetComponent<TextMeshProUGUI>();
-            TextMeshProUGUI profitText = panelTransform.GetChild(7).GetComponent<TextMeshProUGUI>();
-            levelText.text = GetLocalizedValue("LEVEL {0}", level);
-            profitText.text = GetLocalizedValue("PROFIT: +{0}%", GetVehicleProfitMultiplier(haulers[i].name) * 100);
-            panelTransform.GetChild(8).GetChild(1).GetComponent<TextMeshProUGUI>().text = cargo.ToString();
-            panelTransform.GetChild(9).GetChild(1).GetComponent<TextMeshProUGUI>().text = width.ToString();
-            panelTransform.GetChild(10).GetChild(1).GetComponent<Slider>().value = haulerSpeed;
+            panelTransform.GetChild(4).GetComponent<TextMeshProUGUI>().text = haulers[i].name;
+            panelTransform.GetChild(5).GetChild(1).GetComponent<TextMeshProUGUI>().text = cargo.ToString();
+            panelTransform.GetChild(6).GetChild(1).GetComponent<TextMeshProUGUI>().text = width.ToString();
+            panelTransform.GetChild(7).GetChild(1).GetComponent<Slider>().value = haulerSpeed;
 
             // I made some changes, these comments might be wrong
             // Multiply the width and height of the panel image relative to the proportion of 
@@ -244,7 +187,7 @@ public class GarageDelegator : MonoBehaviour, IDataPersistence
             Button buyButton = panelTransform.GetChild(2).GetComponent<Button>();
             // Have to save it as a variable with a local scope, or else it keeps going up and out of bounds
             int index = i;
-            
+            // -1163, -1317, -1471, -1739
             // If vehicle is owned
             if (playerState.CheckVehicleOwnerShip(haulers[i].name)) {
                 PurchasedVehicle(newVehiclePanel, haulers[i]);
@@ -254,11 +197,7 @@ public class GarageDelegator : MonoBehaviour, IDataPersistence
                 buyButton.onClick.AddListener(() => OnBuyButtonClick(newVehiclePanel, haulers[index]));
             }
 
-            // Get the Buy Button component
-            Button upgradeButton = panelTransform.GetChild(4).GetComponent<Button>();
-            // Add an OnClick listener to the button and pass in the prefab of the vehicle
-            upgradeButton.onClick.AddListener(() => OnUpgradeButtonClick(haulers[index].name, panelTransform.GetChild(4), levelText, profitText));
-
+            // For tutorial use
             if (i == 0) {
                 stubbyImage = panelTransform.GetChild(3).GetComponent<Image>();
             }
@@ -273,7 +212,7 @@ public class GarageDelegator : MonoBehaviour, IDataPersistence
 
         // Resize the scroll view content height to fit the rows (top padding + cell height * rows + vertical spacing between cell rows * (rows - 1))
         RectTransform haulersContentRect = haulersContent.GetComponent<RectTransform>();
-        haulersContentRect.sizeDelta = new Vector2(haulersContentRect.sizeDelta.x, 50 + 2500 * haulerRows + 40 * (haulerRows - 1));
+        haulersContentRect.sizeDelta = new Vector2(haulersContentRect.sizeDelta.x, 50 + 2000 * haulerRows + 40 * (haulerRows - 1));
         haulersContent.GetComponent<RectTransform>().sizeDelta = new (0, haulersContentRect.sizeDelta.y);
     }
 
