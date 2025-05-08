9402729932357960f08626b55d11e237cec5a9b7
RiddhRam <ridderp05@gmail.com>
2025-05-08 11:30:02 -0400

    Can now preview different customizations

--- Assets/Scripts/VehicleUpgradeBayManager.cs
+++ Assets/Scripts/VehicleUpgradeBayManager.cs
@@ -40,6 +40,14 @@ public class VehicleUpgradeBayManager : MonoBehaviour, IDataPersistence
     public Image customizationsButtonImage;
     public Image upgradesButtonImage;
 
+    [Header("Customizations Display")]
+    public Outline[] bodyOutlines;
+    public Outline[] drillOutlines;
+
+    private Outline selectedOutline;
+    private Outline equippedBodyOutline;
+    private Outline equippedDrillOutline;
+
     [Header("Other Scripts")]
     public UIDelegation uIDelegation;
     public PlayerState playerState;
@@ -108,6 +116,7 @@ public class VehicleUpgradeBayManager : MonoBehaviour, IDataPersistence
         // Get rid of last vehicle display and create new one that matches the current vehicle
         DestroyPreviousVehicleDisplay();
         CreateNewVehicleDisplay();
+        GenerateCustomizationsDisplays();
 
         uIDelegation.RevealElement(upgradeBayPanel);
         // Stop player from moving;
@@ -117,7 +126,7 @@ public class VehicleUpgradeBayManager : MonoBehaviour, IDataPersistence
     // driller = the gameobject of the driller, not its panel
     // matchActiveDrill = whether or not to update the drill the player is currently using too
     private void MatchGarageDisplayToDrill(int drillerIndex, Transform driller) {
-        bodySprite = GetBodySprite(drillerIndex, driller.name);
+        (bodySprite, _) = GetBodySprite(drillerIndex, driller.name);
         DrillerController currentDrillerController = driller.GetChild(1).GetComponent<DrillerController>();
         
         garageBodyImages[drillerIndex].sprite = bodySprite;
@@ -125,70 +134,71 @@ public class VehicleUpgradeBayManager : MonoBehaviour, IDataPersistence
         drillAnimator = currentDrillerController.GetComponent<Animator>();
 
         if (drillAnimator) {
-            garageDrillImages[drillerIndex].GetComponent<Animator>().runtimeAnimatorController = GetUIDrillAnimator(driller.name);
+            (garageDrillImages[drillerIndex].GetComponent<Animator>().runtimeAnimatorController, _, _) = GetDrillAnimator(driller.name);
         } 
         else {
-            garageDrillImages[drillerIndex].sprite = GetDrillSprite(currentDrillerController.drillTypeIndex, driller.name);
+            (garageDrillImages[drillerIndex].sprite, _) = GetDrillSprite(currentDrillerController.drillTypeIndex, driller.name);
         }
     }
 
     // Find the selected body sprite the user chose
-    private Sprite GetBodySprite(int drillerIndex, string drillName) {
+    private (Sprite bodySprite, int spriteIndex) GetBodySprite(int drillerIndex, string drillName) {
 
         // If they didn't choose any, return the first one, which is supposed to be Vertex
         if (!vehicleCustomizations.ContainsKey(drillName)) {
-            return allBodies[drillerIndex][0];
+            return (allBodies[drillerIndex][0], 0);
         }
 
         string bodySpriteName = vehicleCustomizations[drillName].body;
 
         for (int i = 0; i != allBodies[drillerIndex].Length; i++) {
             if (bodySpriteName == allBodies[drillerIndex][i].name) {
-                return allBodies[drillerIndex][i];
+                return (allBodies[drillerIndex][i], i);
             }
         }
 
         // Fallback
-        return allBodies[drillerIndex][0];
+        return (allBodies[drillerIndex][0], 0);
     }
 
     // Find the selected drill animator the user chose
-    private RuntimeAnimatorController GetUIDrillAnimator(string drillName) {
+    private (RuntimeAnimatorController uiRuntimeAnimatorController, RuntimeAnimatorController runtimeAnimatorController, int animatorIndex) GetDrillAnimator(string drillName) {
+        // Currently only bore drills are animated so it's simple
 
         // If they didn't choose any, return the first one, which is supposed to be Vertex
         if (!vehicleCustomizations.ContainsKey(drillName)) {
-            return boreUIDrills[0];
+            return (boreUIDrills[0], boreDrills[0], 0);
         }
 
         string bodySpriteName = vehicleCustomizations[drillName].body;
 
         for (int i = 0; i != boreUIDrills.Length; i++) {
             if (bodySpriteName == boreUIDrills[i].name) {
-                return boreUIDrills[i];
+                return (boreUIDrills[i], boreDrills[i], i);
             }
         }
 
         // Fallback
-        return boreUIDrills[0];
+        return (boreUIDrills[0], boreDrills[0], 0);
     }
 
-    private Sprite GetDrillSprite(int drillTypeIndex, string drillName) {
+    private (Sprite drillSprite, int spriteIndex) GetDrillSprite(int drillTypeIndex, string drillName) {
 
         // If they didn't choose any, return the first one, which is supposed to be Vertex
         if (!vehicleCustomizations.ContainsKey(drillName)) {
-            return allNormalDrills[drillTypeIndex][0];
+            return (allNormalDrills[drillTypeIndex][0], 0);
         }
 
         string bodySpriteName = vehicleCustomizations[drillName].body;
 
         for (int i = 0; i != allNormalDrills[drillTypeIndex].Length; i++) {
             if (bodySpriteName == allNormalDrills[drillTypeIndex][i].name) {
-                return allNormalDrills[drillTypeIndex][i];
+                return (allNormalDrills[drillTypeIndex][i], i);
             }
         }
 
         // Fallback
-        return allNormalDrills[drillTypeIndex][0];
+        return (allNormalDrills[drillTypeIndex][0], 0);
     }
 
     // Remove the display from the upgrade bay
@@ -215,6 +225,108 @@ public class VehicleUpgradeBayManager : MonoBehaviour, IDataPersistence
         Vector2 pos = rt.anchoredPosition;
         pos.y = -850f;
         rt.anchoredPosition = pos;
+    }
+
+    private void GenerateCustomizationsDisplays() {
+        bool usesAnimatedDrill = drillerController.GetComponent<Animator>();
+
+        for (int i = 0; i != bodyOutlines.Length; i++) {
+            // Reset everything
+            bodyOutlines[i].effectColor = new(1, 1, 1);
+
+            // There should be at least one customization button for each body sprite, so set it to the according one
+            bodyOutlines[i].transform.GetChild(0).GetComponent<Image>().sprite = allBodies[drillerController.drillerIndex][i];
+
+            int index = i;
+            // Add button click listener
+            bodyOutlines[i].transform.GetChild(0).GetComponent<Button>().onClick.AddListener(() => SelectBody(index));
+        }
+
+        // Same as above
+        for (int i = 0; i != drillOutlines.Length; i++) {
+            drillOutlines[i].effectColor = new(1, 1, 1);
+
+            // There should be at least one customization button for each drill animator or sprite
+            drillOutlines[i].transform.GetChild(0).GetComponent<Animator>().runtimeAnimatorController = null;
+            if (usesAnimatedDrill) {
+                drillOutlines[i].transform.GetChild(0).GetComponent<Animator>().runtimeAnimatorController = boreUIDrills[i];
+            } else {
+                drillOutlines[i].transform.GetChild(0).GetComponent<Image>().sprite = allNormalDrills[drillerController.drillTypeIndex][i];
+            }
+
+            int index = i;
+            // Add button click listener
+            drillOutlines[i].transform.GetChild(0).GetComponent<Button>().onClick.AddListener(() => SelectDrill(index));
+        }
+
+        HighlightEquippedOptions(usesAnimatedDrill);
+    }
+
+    private void SelectBody(int index) {
+        // Make the last one white
+        if (selectedOutline) {
+            selectedOutline.effectColor = new(1, 1, 1);
+        }
+
+        // Select the new one and make it green
+        selectedOutline = bodyOutlines[index];
+        selectedOutline.effectColor = new(57/255f, 1, 20/255f);
+
+        // Reset drill sprite
+        drillToCopy.GetChild(2).GetComponent<Animator>().runtimeAnimatorController = null;
+        if (drillerController.GetComponent<Animator>()) {
+            (drillToCopy.GetChild(2).GetComponent<Animator>().runtimeAnimatorController, _, _) = GetDrillAnimator(transform.parent.name);
+        } else {
+            (drillToCopy.GetChild(2).GetComponent<Image>().sprite, _) = GetDrillSprite(drillerController.drillTypeIndex, drillerController.transform.parent.name);
+        }
+
+        // Show preview of driller with this body
+        drillToCopy.GetChild(1).GetComponent<Image>().sprite = allBodies[drillerController.drillerIndex][index];
+        
+        // Rehighlight equipped options to be blue, in case it was made green or white
+        HighlightEquippedOptions(drillerController.GetComponent<Animator>());
+    } 
+
+    private void SelectDrill(int index) {
+        // Make the last one white
+        if (selectedOutline) {
+            selectedOutline.effectColor = new(1, 1, 1);
+        }
+
+        // Select the new one and make it green
+        selectedOutline = drillOutlines[index];
+        selectedOutline.effectColor = new(57/255f, 1, 20/255f);
+
+        // Reset body sprite
+        (drillToCopy.GetChild(1).GetComponent<Image>().sprite, _) = GetBodySprite(drillerController.drillerIndex, drillerController.transform.parent.name);
+        
+        // Show preview of driller with this drill
+        if (drillerController.GetComponent<Animator>()) {
+            drillToCopy.GetChild(2).GetComponent<Animator>().runtimeAnimatorController = boreUIDrills[index];
+        } else {
+            drillToCopy.GetChild(2).GetComponent<Animator>().runtimeAnimatorController = null;
+            drillToCopy.GetChild(2).GetComponent<Image>().sprite = allNormalDrills[drillerController.drillTypeIndex][index];
+        }
+
+        // Rehighlight equipped options to be blue, in case it was made green or white
+        HighlightEquippedOptions(drillerController.GetComponent<Animator>());
+    }
+
+    private void HighlightEquippedOptions(bool usesAnimatedDrill) {
+        // Find the index of the players current option, and then change the outline of that options box to blue
+        int selectedBody;
+        (_, selectedBody) = GetBodySprite(drillerController.drillerIndex, drillerController.transform.parent.name);
+        equippedBodyOutline = bodyOutlines[selectedBody];
+        equippedBodyOutline.effectColor = new(35/255f, 35/255f, 1);
+
+        int selectedDrill;
+        if (usesAnimatedDrill) {
+            (_, _, selectedDrill) = GetDrillAnimator(drillerController.transform.parent.name);
+        } else {
+            (_, selectedDrill) = GetDrillSprite(drillerController.drillTypeIndex, drillerController.transform.parent.name);
+        }
+        equippedDrillOutline = drillOutlines[selectedDrill];
+        equippedDrillOutline.effectColor = new(35/255f, 35/255f, 1);
     }
 
     public void LoadData(GameData data)
@@ -255,7 +367,9 @@ public class VehicleUpgradeBayManager : MonoBehaviour, IDataPersistence
 
             upgradesButtonImage.color = new(1, 1, 1, 90/255f);
             upgradesButtonImage.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new(50/255f, 50/255f, 50/255f, 1);
-        } else {
+            
+        } 
+        else {
             customizationsButtonImage.color = new(1, 1, 1, 90/255f);
             customizationsButtonImage.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new(50/255f, 50/255f, 50/255f, 1);
 
