a5052f947ef64fe4bbf49a093b6aacfda7daa2ef
RiddhRam <ridderp05@gmail.com>
2025-05-10 16:02:26 -0400

    Disabled co-op local

--- Assets/Scripts/GamemodePad.cs
+++ Assets/Scripts/GamemodePad.cs
@@ -3,15 +3,27 @@ using UnityEngine;
 public class GamemodePad : MonoBehaviour
 {
     [SerializeField] UIDelegation uIDelegation;
+    [SerializeField] TutorialManager tutorialManager;
     [SerializeField] JoystickMovement joystickMovement;
     [SerializeField] GameObject gamemodeScreen;
 
     void OnTriggerEnter2D(Collider2D collision) {
-        // Only the player vehicle can open the UI panel on their local game
-        // Also only the drill can activate this pad, not the body
-        if (!collision.transform.parent.parent.name.Contains("Player Vehicle") || !collision.GetComponent<DrillerController>()) {
+
+        if (tutorialManager && !tutorialManager.finishedTutorial) {
+            uIDelegation.ShowError("FINISH THE TUTORIAL FIRST!");
             return;
         }
+
+        // Only the drill/hauler can activate this pad, not the body
+        // Only the player vehicle can open the UI panel on their local game
+        if (!(collision.GetComponent<DrillerController>() || collision.GetComponent<BlasterDriller>() || collision.GetComponent<CreditMagnet>()) || !collision.transform.parent.parent.name.Contains("Player Vehicle")) {
+            return;
+        }
+
+        // Ignore if the Rigidbody2D is essentially stationary, this means the game just loaded
+        var rb2d = collision.attachedRigidbody;
+        if (rb2d != null && rb2d.velocity.sqrMagnitude < 0.01f)
+            return;
 
         uIDelegation.HideAll();
         uIDelegation.RevealElement(gamemodeScreen);
--- Assets/Scripts/Special Modes/Ore Blaster/BlasterDriller.cs
+++ Assets/Scripts/Special Modes/Ore Blaster/BlasterDriller.cs
@@ -6,7 +6,7 @@ public class BlasterDriller : MonoBehaviour
 
     void Start() {
         // Do this so front wheels are found
-        PlayerMovement playerMovement = transform.parent.GetComponent<PlayerMovement>();
+        PlayerMovement playerMovement = transform.parent.parent.GetComponent<PlayerMovement>();
         playerMovement.SetSpeed(playerMovement.GetSpeed());
     }
 
