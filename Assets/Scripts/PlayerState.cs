169149a4fd72436891f42d9b80582484e728d403
RiddhRam <ridderp05@gmail.com>
2025-05-14 21:47:59 -0400

    Minor changes

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -346,35 +346,25 @@ public class PlayerState : MonoBehaviour, IDataPersistence
     // overestimate their money and buy something they can't afford
     public string FormatPrice(BigInteger price)
     {
-        if (price >= 1_000_000_000_000_000_000)
+        if (price >= 1_000_000_000_000)
         {
-            // Truncate to 3 decimal places and format with "Qu"
-            return (Mathf.Floor((float) price / 1_000_000_000_000_000_000f * 1000) / 1000).ToString("0.###") + "Qu";
-        }
-        else if (price >= 1_000_000_000_000_000)
-        {
-            // Truncate to 3 decimal places and format with "Q"
-            return (Mathf.Floor((float) price / 1_000_000_000_000_000f * 1000) / 1000).ToString("0.###") + "Q";
-        }
-        else if (price >= 1_000_000_000_000)
-        {
-            // Truncate to 3 decimal places and format with "T"
-            return (Mathf.Floor((float) price / 1_000_000_000_000f * 1000) / 1000).ToString("0.###") + "T";
+            // Truncate to 2 decimal places and format with "T"
+            return (Mathf.Floor((float) price / 1_000_000_000_000f * 1000) / 1000).ToString("0.##") + "T";
         }
         else if (price >= 1_000_000_000)
         {
-            // Truncate to 3 decimal places and format with "B"
-            return (Mathf.Floor((float) price / 1_000_000_000f * 1000) / 1000).ToString("0.###") + "B";
+            // Truncate to 2 decimal places and format with "B"
+            return (Mathf.Floor((float) price / 1_000_000_000f * 1000) / 1000).ToString("0.##") + "B";
         }
         else if (price >= 1_000_000)
         {
-            // Truncate to 3 decimal places and format with "M"
-            return (Mathf.Floor((float) price / 1_000_000f * 1000) / 1000).ToString("0.###") + "M";
+            // Truncate to 2 decimal places and format with "M"
+            return (Mathf.Floor((float) price / 1_000_000f * 1000) / 1000).ToString("0.##") + "M";
         }
         else if (price >= 1_000)
         {
-            // Truncate to 3 decimal places and format with "K"
-            return (Mathf.Floor((float) price / 1_000f * 1000) / 1000).ToString("0.###") + "K";
+            // Truncate to 2 decimal places and format with "K"
+            return (Mathf.Floor((float) price / 1_000f * 1000) / 1000).ToString("0.##") + "K";
         }
 
         // Return the original price as a string for smaller numbers
