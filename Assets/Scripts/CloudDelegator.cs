a9e5553cb244bf8b686aa1186fe02434ce993a64
RiddhRam <ridderp05@gmail.com>
2025-07-05 21:43:53 -0400

    6.1.3 (131)

--- Assets/Scripts/CloudDelegator.cs
+++ Assets/Scripts/CloudDelegator.cs
@@ -55,7 +55,7 @@ public class CloudDelegator : MonoBehaviour
     public TMP_InputField signUpEmail;
     public TMP_InputField signUpPassword;
 
-    private readonly int currentVersionNumber = 130;
+    private readonly int currentVersionNumber = 131;
     private bool notSinglePlayerScene = false;
     public bool doingSigninProcess = false;
 
--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -149,6 +149,6 @@ public class GameData
 
         this.bp = 0;
         
-        this.id = 130;
+        this.id = 131;
     }
 }
